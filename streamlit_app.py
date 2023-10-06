c95053aa3f664b21962e0b85e083ee363d8aa782
pinedsf <147145148+pinedsf@users.noreply.github.com>
2023-10-06 16:22:02 +0700

    Update set fruit_index to fruit_name

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ st.text('🥑 🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Let's put a pick list here so they can pick the fruit they want to include 
+my_fruit_list = my_fruit_list.set_index('Fruit')
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
